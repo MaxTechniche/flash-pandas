7989ae6342d3b956a2d6608bd7dd26a5ce518e11
MaxTechniche <Max.Techniche@gmail.com>
2020-11-23 22:59:19 -0700

    Working on DB

--- flashpandas/dbmodels.py
+++ flashpandas/dbmodels.py
@@ -1,4 +1,3 @@
-from typing import Sequence
 from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
@@ -6,6 +5,12 @@ DB = SQLAlchemy()
 class User(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     username = DB.Column(DB.String(30), nullable=True)
+
+class Question(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    answer_text = DB.Column(DB.String(500))
+
+
 
 
 # TODO Password encrypting (bcrypt)
