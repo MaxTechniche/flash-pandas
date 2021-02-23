00074c4f08f6839d56a25ad514e96645211e48ba
Jacob <max.techniche@gmail.com>
2021-02-22 21:24:46 -0700

    Revert "Revert "formatting""
    This reverts commit 9ecf09db2e80f203e799675942fee2e40fdb497b.

--- flashpandas/pages/home.py
+++ flashpandas/pages/home.py
@@ -16,41 +16,46 @@ layout = dbc.Col(
             Click on learn to view flash cards already created.  
             You must have an account to create a card, but you do not need an account to view public flash cards.""",
         ),
-        dbc.Label("If you'd like to submit a question, comment, concern, or complaint, please do so below. Include as much detail as you can. Email is NOT required unless you hope for a reply."),
-        dbc.Input(id='contact-email', placeholder='Email', style={'max-width': '300px'}),
-        dbc.Textarea(id='contact-text', placeholder='Comment'),
-        html.Div([
-            dcc.Markdown(id='contact-code'),
-            dbc.Button('Submit', id='contact-submit')
-        ], style={'text-align': 'center'})
+        dbc.Label(
+            "If you'd like to submit a question, comment, concern, or complaint, please do so below. Include as much detail as you can. Email is NOT required unless you hope for a reply."
+        ),
+        dbc.Input(
+            id="contact-email", placeholder="Email", style={"max-width": "300px"}
+        ),
+        dbc.Textarea(id="contact-text", placeholder="Comment"),
+        html.Div(
+            [
+                dcc.Markdown(id="contact-code"),
+                dbc.Button("Submit", id="contact-submit"),
+            ],
+            style={"text-align": "center"},
+        ),
     ]
 )
 
+
 @APP.callback(
-    Output('contact-code', 'children'),
-    Input('contact-submit', 'n_clicks'),
-    [State('contact-text', 'value'),
-    State('contact-email', 'value')]
+    Output("contact-code", "children"),
+    Input("contact-submit", "n_clicks"),
+    [State("contact-text", "value"), State("contact-email", "value")],
 )
 def submit_comment(n_clicks, comment, email):
     if n_clicks:
         if not comment:
-            return 'No text detected'
+            return "No text detected"
 
         code = comments.insert_one(
             {
-                'comment': comment, 
-                'email': users.find_one(
-                    {
-                        'username': session.get('username')
-                    }
-                )['email']
-            })
+                "comment": comment,
+                "email": users.find_one({"username": session.get("username")})["email"],
+            }
+        )
 
-        return \
-            """
+        return """
             Submission Successful.  
             Here is your comment ID:  
-            """ + str(code.inserted_id)
-    
-    return ''
+            """ + str(
+            code.inserted_id
+        )
+
+    return ""
--- flashpandas/pages/signup.py
+++ flashpandas/pages/signup.py
@@ -10,103 +10,105 @@ from flask import session
 from pymongo.errors import DuplicateKeyError
 from flashpandas.app import APP, users, cards
 
-layout = \
-    html.Div([
-        html.Div('Create Account', style={'text-align': 'center', 'font-size': '20px'}),
+layout = html.Div(
+    [
+        html.Div("Create Account", style={"text-align": "center", "font-size": "20px"}),
         dbc.Col(
             [
-                html.Form([
-
-                # dbc.Label('Email: needed to reset password', id='email-label'),
-                # dbc.Input(
-                #     id='email-entry',
-                #     style={'max-width': '250px', 'margin-bottom': '20px'}
-                # ),
-                dbc.Label('Username (Display Name): 6-30 characters'),
-                dbc.Input(
-                    id='username-signup-entry',
-                    style={'max-width': '250px', 'margin-bottom': '20px'}
-                ),
-                dbc.Label('Email:'),
-                dbc.Input(
-                    id='email-signup-entry',
-                    style={'max-width': '250px', 'margin-bottom': '20px'}
-                ),
-                dbc.Label('Password: 6-30 characters'),
-                dbc.Input(
-                    id='password-signup-entry',
-                    type='password',
-                    style={'max-width': '250px'}
-                ),
-                dbc.Checkbox(
-                    id='pass-signup-toggle'
-                ),
-                dbc.Label('Show Password', style={'margin-left': '5px'}),
-                html.Div(),
-                dbc.Button(
-                    'Create Account',
-                    id='signup-button',
-                    color='success'
-                ),
-                dbc.Label(
-                    children=[''],
-                    id='info-signup-label',
-                    style={'margin-left': '10px'}
+                html.Form(
+                    [
+                        # dbc.Label('Email: needed to reset password', id='email-label'),
+                        # dbc.Input(
+                        #     id='email-entry',
+                        #     style={'max-width': '250px', 'margin-bottom': '20px'}
+                        # ),
+                        dbc.Label("Username (Display Name): 6-30 characters"),
+                        dbc.Input(
+                            id="username-signup-entry",
+                            style={"max-width": "250px", "margin-bottom": "20px"},
+                        ),
+                        dbc.Label("Email:"),
+                        dbc.Input(
+                            id="email-signup-entry",
+                            style={"max-width": "250px", "margin-bottom": "20px"},
+                        ),
+                        dbc.Label("Password: 6-30 characters"),
+                        dbc.Input(
+                            id="password-signup-entry",
+                            type="password",
+                            style={"max-width": "250px"},
+                        ),
+                        dbc.Checkbox(id="pass-signup-toggle"),
+                        dbc.Label("Show Password", style={"margin-left": "5px"}),
+                        html.Div(),
+                        dbc.Button(
+                            "Create Account", id="signup-button", color="success"
+                        ),
+                        dbc.Label(
+                            children=[""],
+                            id="info-signup-label",
+                            style={"margin-left": "10px"},
+                        ),
+                    ]
                 )
-                ])
             ]
-        ),],
-        style={'text-alignment': 'center'}
-    )
+        ),
+    ],
+    style={"text-alignment": "center"},
+)
 
 
 @APP.callback(
-    Output('password-signup-entry', 'type'),
-    Input('pass-signup-toggle', 'checked')
+    Output("password-signup-entry", "type"), Input("pass-signup-toggle", "checked")
 )
 def toggle_signup_password_visibility(checked):
     if checked:
-        return 'text'
+        return "text"
     else:
-        return 'password'
+        return "password"
+
 
 @APP.callback(
-    Output('info-signup-label', 'children'),
-    Input('signup-button', 'n_clicks'),
-    [State('email-signup-entry', 'value'),
-    State('username-signup-entry', 'value'),
-    State('password-signup-entry', 'value')]
+    Output("info-signup-label", "children"),
+    Input("signup-button", "n_clicks"),
+    [
+        State("email-signup-entry", "value"),
+        State("username-signup-entry", "value"),
+        State("password-signup-entry", "value"),
+    ],
 )
 def check_signup(n_clicks, email, username, password):
     if n_clicks:
         if not username or not password or not email:
-            return 'Missing some information'
+            return "Missing some information"
 
         if len(username) > 30:
-            return 'Username too long'
+            return "Username too long"
 
         if len(username) < 6:
-            return 'Username too short'
+            return "Username too short"
 
         if len(password) > 30:
-            return 'Password too long'
+            return "Password too long"
 
         if len(password) < 6:
-            return 'Password too short'
+            return "Password too short"
 
         try:
-            users.insert_one({
-                'username': username, 
-                'email': email,
-                'password': hashpw(bytes(password, 'utf-8'), gensalt()),
-                'creation_time': datetime.datetime.utcnow()
-            })
-            session['username'] = username
+            users.insert_one(
+                {
+                    "username": username,
+                    "email": email,
+                    "password": hashpw(bytes(password, "utf-8"), gensalt()),
+                    "creation_time": datetime.datetime.utcnow(),
+                }
+            )
+            session["username"] = username
         except Exception as e:
-            if users.find_one({'username': username}):
-                return 'Username already in use'
-            elif users.find_one({'email': email}):
-                return 'Email already in use'
-        
-        return dcc.Location('url', '/profile')
-    return ''
+            if users.find_one({"username": username}):
+                return "Username already in use"
+            elif users.find_one({"email": email}):
+                return "Email already in use"
+
+        return dcc.Location("url", "/profile")
+    return ""
